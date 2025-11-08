49f85f64e51a7ce94caf81873921839d68fe8afb
Jo_z 404 <lucasjosecafeangelofigueiredo@gmail.com>
2025-11-08 10:05:12 -0400

    Create Exercicio02.py
     S O C O R R O

---
+++ Exercicio02.py
@@ -0,0 +1,4 @@
+eu sinto algo horrivel no meu peito sinto dos risadas dos milhares ao meu redor mesmo q saiba q essa nao e verdade
+eu sinto eu sou a pida a vontade de me entregar aos delirios e me esconder na insaniidade so almenta
+numero = int(input('Digite um numero '))
+print ('o numero informado foi: ', numero)
