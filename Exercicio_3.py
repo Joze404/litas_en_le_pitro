61a3a5a911f35e314393c478990c359c3cdf1cb6
Jo_z 404 <lucasjosecafeangelofigueiredo@gmail.com>
2025-11-08 10:13:13 -0400

    Create Exercicio_3.py
    bum bum

---
+++ Exercicio_3.py
@@ -0,0 +1,8 @@
+
+numero = int(input('Digite um numero '))
+numerokys = int(input('Digite numero: '))
+soma = numero + numerokys
+
+print ('o numero informado foi: ', soma)
+
+'''o q eu escrevi no exercico dois era piada nao chama a policia porfavor'''
