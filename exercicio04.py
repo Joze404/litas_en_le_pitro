9dab078527bfd88c68e5d8ce91cdbe0820ae0549
Jo_z 404 <lucasjosecafeangelofigueiredo@gmail.com>
2025-11-08 10:23:38 -0400

    Create exercicio04.py
    I MISSSSSS THE QUIETTTTTTTTTT

---
+++ exercicio04.py
@@ -0,0 +1,4 @@
+nota1 = int(input('Digite um numero: '))
+nota2 = int(input('Digite numero: '))
+
+eu queria colocar algo encrgado mas to sem ideia porque quero ir pra casa jogar jogo do macaco estourando balao
